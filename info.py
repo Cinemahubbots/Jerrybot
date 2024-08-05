a9f5955b649704473530d30b1577a4ea8be3f32f
RolexPc <157006183+RolexPc@users.noreply.github.com>
2024-08-05 22:13:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ auth_grp = environ.get('AUTH_GROUP')
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_NAME = environ.get('DATABASE_NAME', "Autofilterv2")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Jerrycbbot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Jerrycbbot:Jerrycbbot@cluster0.hxkjmbw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://saniyav1:saniyav1@cluster0.bchlaxl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
