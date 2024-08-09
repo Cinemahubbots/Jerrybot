6168bb13b60fd678e73992cbdba5c0bc700b9a3b
RolexPc <157006183+RolexPc@users.noreply.github.com>
2024-08-09 15:03:44 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -804,7 +804,7 @@ async def auto_filter(client, msg, spoll=False):
     else:
         await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
     if spoll:
-        await msg.message.delete()
+        #await msg.message.delete()
 	    
 async def advantage_spell_chok(msg):
     mv_rqst = msg.text
