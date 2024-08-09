815db4c0dfb3a030ea9d37fff4e1b28679f9721d
RolexPc <157006183+RolexPc@users.noreply.github.com>
2024-08-09 12:32:45 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -270,6 +270,7 @@ async def channel_info(bot, message):
         os.remove(file)
 
 
+
 @Client.on_message(filters.command('logs') & filters.user(ADMINS))
 async def log_file(bot, message):
     """Send log file"""
